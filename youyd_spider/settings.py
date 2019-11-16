c751ed272fd0f8fdc9861e7fd839fba352318fef
company <company@qq.com>
2019-11-16 19:36:10 +0800

    :globe_with_meridians: Internationalization and localization.

--- youyd_spider/settings.py
+++ youyd_spider/settings.py
@@ -2,79 +2,64 @@
 
 import os
 
-# Scrapy settings for youyd_spider project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-
 BOT_NAME = 'youyd_spider'
 
 SPIDER_MODULES = ['youyd_spider.spiders']
 NEWSPIDER_MODULE = 'youyd_spider.spiders'
 
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
+# 通过在用户代理上标识自己（和您的网站）来负责任地进行爬网
 #USER_AGENT = 'youyd_spider (+http://www.yourdomain.com)'
 
-# Obey robots.txt rules
+# 遵守robots.txt规则
 ROBOTSTXT_OBEY = False
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
+# 配置Scrapy执行的最大并发请求数（默认值：16）
 #CONCURRENT_REQUESTS = 32
 
-# Configure a delay for requests for the same website (default: 0)
-# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
+# 配置同一网站的请求延迟（默认值：0）
+# DOWNLOAD_DELAY = 3
+# 下载延迟设置将仅接受以下一项
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
-# Disable cookies (enabled by default)
+# 禁用cookie（默认情况下启用）
 COOKIES_ENABLED = True
 
-# Disable Telnet Console (enabled by default)
+# 禁用Telnet控制台（默认情况下启用）
 #TELNETCONSOLE_ENABLED = False
 
-# Override the default request headers:
+# 覆盖默认的请求头：
 #DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
 #}
 
-# Enable or disable spider middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+# 启用或禁用蜘蛛中间件
 #SPIDER_MIDDLEWARES = {
 #    'youyd_spider.middlewares.ArticlespiderSpiderMiddleware': 543,
 #}
 
-# Enable or disable downloader middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+# 启用或禁用下载器中间件
 DOWNLOADER_MIDDLEWARES = {
    # 'youyd_spider.middlewares.MyCustomDownloaderMiddleware': 543,
     'youyd_spider.middlewares.rotate_user_agent.RandomUserAgentMiddleware': 400,
 }
 JSONRPC_ENABLED=True
 
-# Enable or disable extensions
-# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
+# 启用或禁用扩展
 #EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
 #}
 
 
-# Configure item pipelines
-# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
+# 配置项目管道 item pipelines
 # 指定spider运行流程 执行哪些pipeline
 ITEM_PIPELINES = {
    'youyd_spider.pipelines.JsonExporterPipleline': 2,
    #  'scrapy.pipelines.images.ImagesPipeline': 1,
    #  'youyd_spider.pipelines.ArticleImagePipeline': 2,
-     'youyd_spider.pipelines.MysqlTwistedPipline': 5,
+   #   'youyd_spider.pipelines.MysqlTwistedPipline': 5,
    #  'youyd_spider.pipelines.ElasticSearchPipeline': 5
 
 }
@@ -90,22 +75,21 @@ print (os.path.join(BASE_DIR, 'AritcleSpider'))
 # IMAGES_MIN_HEIGHT = 100
 # IMAGES_MIN_WIDTH = 100
 
-# Enable and configure the AutoThrottle extension (disabled by default)
+# 启用和配置AutoThrottle扩展（默认情况下禁用）
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
-# The initial download delay
+# 初始下载延迟
 AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
+# 高延迟时要设置的最大下载延迟
 AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
+# Scrapy应该并行发送的平均请求数
+# 每个远程服务器
 AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
+# 启用显示收到的每个响应的节流统计信息：
 AUTOTHROTTLE_DEBUG = False
 DOWNLOAD_DELAY = 3
 
-# Enable and configure HTTP caching (disabled by default)
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+# 启用和配置HTTP缓存（默认情况下禁用）
 #HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = 'httpcache'
